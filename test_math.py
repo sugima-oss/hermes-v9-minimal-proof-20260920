3151b807054da2f7febc64647cd2d57e6440f5b5
sugima-oss <sugima.m@gmail.com>
2026-09-20 19:45:50 +0900

    Add subtract function and test (with defect)

--- test_math.py
+++ test_math.py
@@ -1,4 +1,8 @@
-from math import multiply
+from arithmetic import multiply, subtract
 
 def test_multiply():
     assert multiply(6, 7) == 42
+
+
+def test_subtract():
+    assert subtract(10, 4) == 6
